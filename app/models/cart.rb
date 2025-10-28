8d38d4db3cce3089b47bc7ba0dbb8ffe81ba42a1
Davi Severino <daviseverino96@gmail.com>
2025-10-28 00:25:47 +0000

    update models to represent new schema

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,8 @@
 class Cart < ApplicationRecord
+  enum status: { open: 0, abandoned: 1 }
+
+  has_many :cart_items, dependent: :destroy
+
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
